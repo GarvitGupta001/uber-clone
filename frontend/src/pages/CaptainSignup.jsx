9a84d55c7f3a96166fa7194860aed3670d945060
Garvit Gupta <garvitgupta2606@ce.du.ac.in>
2025-06-24 19:11:06 +0530

    usersignup and captain signup UI

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -1,8 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import LogoHeader from '../components/LogoHeader'
 
 const CaptainSignup = () => {
+  const [user, setUser] = useState({
+    fullname: {
+      firstname: "",
+      lastname: ""
+    },
+    email: "",
+    password: "",
+    vehicle: {
+      color: "",
+      number: "",
+      type: "",
+      capacity: ""
+    }
+  })
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({ email: "", password: "", fullname: { firstname: "", lastname: "" }, vehicle: { color: "", number: "", type: "", capacity: "" } });
+  }
+
   return (
-    <div>CaptainSignup</div>
+    <div className='h-screen flex flex-col justify-between pt-2 px-4 pb-8'>
+      <div>
+        <LogoHeader />
+        <form action=""
+          className='flex flex-col items-center justify-start gap-4'
+          onSubmit={handleSignup}>
+          <div className='w-full'>
+            <h3 className='text-lg mb-2 font-medium'>Personal Information</h3>
+            <div className='flex justify-between'>
+              <input type="text"
+                required
+                placeholder='First Name'
+                className='bg-[#eeeeee] px-3 py-2 rounded text-base font-medium placeholder:text-sm placeholder:text-gray-400 placeholder:font-medium mb-2 w-[49%]'
+                value={user.fullname.firstname}
+                onChange={(e) => setUser({ ...user, fullname: { ...user.fullname, firstname: e.target.value } })} />
+              <input type="text"
+                placeholder='Last Name'
+                className='bg-[#eeeeee] px-3 py-2 rounded text-base font-medium placeholder:text-sm placeholder:text-gray-400 placeholder:font-medium mb-2 w-[49%]'
+                value={user.fullname.lastname}
+                onChange={(e) => setUser({ ...user, fullname: { ...user.fullname, lastname: e.target.value } })} />
+            </div>
+            <input type="email"
+              required
+              placeholder='user_mail@example.com'
+              className='bg-[#eeeeee] w-full px-3 py-2 rounded text-base font-medium placeholder:text-sm placeholder:text-gray-400 placeholder:font-medium mb-2'
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })} />
+            <input type="password"
+              required
+              placeholder='Password'
+              className='bg-[#eeeeee] w-full px-3 py-2 rounded text-base font-medium placeholder:text-sm placeholder:text-gray-400 placeholder:font-medium mb-2'
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })} />
+          </div>
+          <div className='w-full'>
+            <h3 className='text-lg mb-2 font-medium'>Vehicle Information</h3>
+            <input type="text"
+              required
+              placeholder='Color'
+              className='bg-[#eeeeee] w-full px-3 py-2 rounded text-base font-medium placeholder:text-sm placeholder:text-gray-400 placeholder:font-medium mb-2'
+              value={user.vehicle.color}
+              onChange={(e) => setUser({ ...user, vehicle: { ...user.vehicle, color: e.target.value } })} />
+            <input type="text"
+              required
+              placeholder='Number'
+              className='bg-[#eeeeee] w-full px-3 py-2 rounded text-base font-medium placeholder:text-sm placeholder:text-gray-400 placeholder:font-medium mb-2'
+              value={user.vehicle.number}
+              onChange={(e) => setUser({ ...user, vehicle: { ...user.vehicle, number: e.target.value } })} />
+            <input type="text"
+              required
+              placeholder='Type'
+              className='bg-[#eeeeee] w-full px-3 py-2 rounded text-base font-medium placeholder:text-sm placeholder:text-gray-400 placeholder:font-medium mb-2'
+              value={user.vehicle.type}
+              onChange={(e) => setUser({ ...user, vehicle: { ...user.vehicle, type: e.target.value } })} />
+            <input type="text"
+              required
+              placeholder='Capacity'
+              className='bg-[#eeeeee] w-full px-3 py-2 rounded text-base font-medium placeholder:text-sm placeholder:text-gray-400 placeholder:font-medium mb-2'
+              value={user.vehicle.capacity}
+              onChange={(e) => setUser({ ...user, vehicle: { ...user.vehicle, capacity: e.target.value } })} />
+          </div>
+
+          <button type="submit"
+            className='bg-black text-white px-4 py-2 rounded w-full active:bg-gray-800'>Sign Up</button>
+        </form>
+        <p className='text-center'>
+          Already a captain? <Link to="/captain-login" className='text-blue-700 active:text-violet-900'>Log in</Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -3,12 +3,19 @@ import { Link } from 'react-router-dom'
 import LogoHeader from '../components/LogoHeader'
 
 const UserSignup = () => {
-    const [user, setUser] = useState({ email: "", password: "" })
+    const [user, setUser] = useState({
+        fullname: {
+            firstname: "",
+            lastname: ""
+        },
+        email: "",
+        password: ""
+    })
 
     const handleSignup = async (e) => {
         e.preventDefault();
         console.log(user);
-        setUser({ ...user, email: "", password: "" });
+        setUser({ email: "", password: "", fullname: { firstname: "", lastname: "" } });
     }
 
     return (
@@ -17,8 +24,21 @@ const UserSignup = () => {
                 <LogoHeader />
                 <form action=""
                     className='flex flex-col items-center justify-start gap-4'
-                    onSubmit={handleSignup}
-                >
+                    onSubmit={handleSignup}>
+                    <div className='flex justify-between'>
+
+                        <input type="text"
+                            required
+                            placeholder='First Name'
+                            className='bg-[#eeeeee] px-3 py-2 rounded text-base font-medium placeholder:text-sm placeholder:text-gray-400 placeholder:font-medium mb-2 w-[49%]'
+                            value={user.fullname.firstname}
+                            onChange={(e) => setUser({ ...user, fullname: { ...user.fullname, firstname: e.target.value } })} />
+                        <input type="text"
+                            placeholder='Last Name'
+                            className='bg-[#eeeeee] px-3 py-2 rounded text-base font-medium placeholder:text-sm placeholder:text-gray-400 placeholder:font-medium mb-2 w-[49%]'
+                            value={user.fullname.lastname}
+                            onChange={(e) => setUser({ ...user, fullname: { ...user.fullname, lastname: e.target.value } })} />
+                    </div>
                     <input type="email"
                         required
                         placeholder='user_mail@example.com'
@@ -32,16 +52,11 @@ const UserSignup = () => {
                         value={user.password}
                         onChange={(e) => setUser({ ...user, password: e.target.value })} />
                     <button type="submit"
-                        className='bg-black text-white px-4 py-2 rounded w-full active:bg-gray-800'>Login</button>
+                        className='bg-black text-white px-4 py-2 rounded w-full active:bg-gray-800'>Sign Up</button>
                 </form>
                 <p className='text-center'>
-                    Don't have an account? <Link to="/signup" className='text-blue-700 active:text-violet-900'>Sign up</Link>
+                    Already have an account? <Link to="/login" className='text-blue-700 active:text-violet-900'>Log in</Link>
                 </p>
-            </div>
-            <div className='text-center'>
-                <Link to="/captain-login">
-                    <button className='bg-emerald-600 active:bg-emerald-800 text-white px-4 py-2 rounded w-full inline-block'>Login as Captain</button>
-                </Link>
             </div>
         </div>
     )
