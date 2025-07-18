0d00c304cc5aaff35818931c71d92a96563a950d
Garvit Gupta <garvitgupta2606@ce.du.ac.in>
2025-07-18 19:49:18 +0530

    minor bug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import UserContext from './context/userContext.jsx'
+import UserContext from './context/UserContext.jsx'
 import CaptainContext from './context/CaptainContext.jsx'
 import SocketContext from './context/SocketContext.jsx'
 
